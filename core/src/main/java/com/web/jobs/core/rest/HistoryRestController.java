ff67f7ae9f54a08d932c4ebeb52cc8b9ec83abdd
LucianCristianFilip <luciancristian.filip@gmail.com>
2018-02-17 18:56:44 +0200

    WJ-0008 Create SpringController for each DAO

--- core/src/main/java/com/web/jobs/core/rest/HistoryRestController.java
+++ core/src/main/java/com/web/jobs/core/rest/HistoryRestController.java
@@ -1,7 +1,24 @@
 package com.web.jobs.core.rest;
 
+import com.web.jobs.core.service.HistoryService;
+import com.web.jobs.persistence.dto.HistoryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/wj/history")
 public class HistoryRestController {
+
+    @Autowired
+    private HistoryService historyService;
+
+    @PostMapping
+    public ResponseEntity createHistory(@RequestBody HistoryDTO history) {
+        historyService.insert(history.getUserId(), history.getJobId(), history.getRatingId(), history.getEmployeeId(), history.getPrice(), history.getTimestamp());
+
+        return ResponseEntity.ok().build();
+    }
+
 }

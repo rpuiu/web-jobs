2c6ac751d1ec6592970dcf04983bfc5036ac17b4
Lucian Filip <lfilip@luxoft.com>
2018-04-11 21:24:42 +0300

    MERGEE

--- core/src/main/resources/static/app.js
+++ core/src/main/resources/static/app.js
@@ -1,21 +1,16 @@
 (function (){
     'use strict';
 
-  var app = angular.module('app', ['ngRoute']);
-    
-    app.config(['$routeProvider', function($routeProvider){
-        $routeProvider
-            .when('/', {
-                templateUrl: '/views/test.html',
-                controller: 'MainController.js'
-            })
-            .when('/history', {
-                templateUrl: '/views/history.html',
-                controller: 'HistoryController.js'
-            })
-            .otherwise({
-                redirectTo: 'www.google.com'
-            });
-    }]);
-    
+var app = angular.module("app", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "views/green.html",
+        controller: "HistoryController"
+    })
+    .when("/red", {
+        templateUrl : "views/red.html",
+        controller: "MainController"
+    })
+});
 })();

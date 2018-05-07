23d022b9e430ec3989b32ef6fc23239cb6ef75ed
rpuiu <raz.puiu@gmail.com>
2018-05-07 21:21:02 +0300

    WJ-0019 Use data from Sign Up form to create new users - calling rest controller on submit form adds new user to the DB

--- core/src/main/java/com/web/jobs/core/rest/UserRestController.java
+++ core/src/main/java/com/web/jobs/core/rest/UserRestController.java
@@ -1,16 +1,13 @@
 package com.web.jobs.core.rest;
 
 import com.web.jobs.core.service.UserService;
+import com.web.jobs.persistence.entity.PersonEntity;
 import com.web.jobs.persistence.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -51,5 +48,4 @@ public class UserRestController {
     public List getAll() {
         return userService.getAll();
     }
-    
 }
--- core/src/main/resources/static/controllers/UserController.js
+++ core/src/main/resources/static/controllers/UserController.js
@@ -2,12 +2,32 @@
     'use strict';
 
     angular.module('app')
-        .controller('UserController', UserController);
+        .controller('UserController', UserController );
 
-    UserController.$inject = ['$http'];
+    UserController.$inject = ['$http', '$scope'];
 
-    function UserController($http) {
+    function UserController($http, $scope) {
         console.log("UserController.");
+
+        $scope.addUser = function(){
+            var successCallBack = function(response){
+                // success response found from server
+            };
+
+            var errorCallBack = function(response){
+                // error response found from server
+            };
+                var data = {
+                    personEntity: {
+                        name: $scope.name,
+                        surname: $scope.surname,
+                        username: $scope.username,
+                        phone: $scope.phone,
+                        email: $scope.email
+                    }
+                };
+                $http.post('/api/user/add', data).then(successCallBack, errorCallBack);
+        };
     }
 
 })();

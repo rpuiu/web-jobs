2124dadecfd891a80c5d276687831e88fef220a6
LucianCristianFilip <luciancristian.filip@gmail.com>
2018-05-23 22:45:32 +0300

    WJ-0017 Create the landing page

---
+++ core/src/main/java/com/web/jobs/core/rest/HomeController.java
@@ -0,0 +1,25 @@
+package com.web.jobs.core.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+    //~ Instance fields 
+    //~ ----------------------------------------------------------------------------------------------------------------
+
+    @GetMapping({"/", "index"})
+    public String getIndex(Model model) {
+        return "index";
+    }
+
+    @GetMapping("secured")
+    public String secured() {
+        return "secured";
+    }
+
+}
